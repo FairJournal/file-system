7bd8e6de98ea1b8dd416e137db786c43975468c6
igar1991 <iiggaarr19912004@gmail.com>
2023-07-06 20:43:26 +0300

    feat: exported actions interfaces

--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,33 @@ import { Tree } from './file-system/interfaces/tree'
 import { Update } from './file-system/update/update'
 import { Action, ActionType } from './file-system/update/interfaces/action'
 import { UpdateDataSigned } from './file-system/update/interfaces/update-data-signed'
+import {
+  createAddDirectoryAction,
+  AddDirectoryActionData,
+  AddDirectoryAction,
+} from './file-system/update/interfaces/add-directory-action'
+import { createAddUserAction, AddUserActionData, AddUserAction } from './file-system/update/interfaces/add-user-action'
+import { createAddFileAction, AddFileActionData, AddFileAction } from './file-system/update/interfaces/add-file-action'
 
-export { FileSystem, Directory, File, Tree, Update, Action, UpdateDataSigned, ActionType }
+export {
+  FileSystem,
+  Directory,
+  File,
+  Tree,
+  Update,
+  Action,
+  UpdateDataSigned,
+  ActionType,
+  createAddDirectoryAction,
+  AddDirectoryActionData,
+  AddDirectoryAction,
+  createAddUserAction,
+  AddUserActionData,
+  AddUserAction,
+  createAddFileAction,
+  AddFileActionData,
+  AddFileAction,
+}
 
 declare global {
   interface Window {
