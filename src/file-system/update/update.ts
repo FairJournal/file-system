3bc714e9b5f0fd6201bebfadbf017111e0bce92d
igar1991 <iiggaarr19912004@gmail.com>
2023-07-06 21:21:50 +0300

    refactor: ability to change user address

--- src/file-system/update/update.ts
+++ src/file-system/update/update.ts
@@ -29,14 +29,20 @@ export class Update {
   private actions: Action[] = []
 
   /**
+   * Address or public key of the user
+   * @private
+   */
+  private userAddress = ''
+
+  /**
    * Creates update
    *
    * @param projectName Name of the project
    * @param userAddress Address or public key of the user
    * @param id Id of the update
    */
-  constructor(public projectName: string, private readonly userAddress: string, id?: number) {
-    this.userAddress = userAddress.toLowerCase()
+  constructor(public projectName: string, userAddress?: string, id?: number) {
+    this.setUserAddress(userAddress || '')
 
     if (id !== undefined) {
       this.setId(id)
@@ -108,6 +114,15 @@ export class Update {
    */
   getUserAddress(): string {
     return this.userAddress
+  }
+
+  /**
+   * Sets address of the user
+   *
+   * @param userAddress Address of the user
+   */
+  setUserAddress(userAddress: string): void {
+    this.userAddress = userAddress.toLowerCase()
   }
 
   /**
